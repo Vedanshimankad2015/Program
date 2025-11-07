091974f06b212c67848fdf94bca0311415eebcca
Vedanshimankad2015 <vedanshimankad11@gmail.com>
2025-11-07 19:58:52 +0530

    dataype.py

---
+++ Datatype.py
@@ -0,0 +1,19 @@
+name="Vedanshi"
+age=10
+student=True
+weight=38.5
+print("name:",name)
+print("datatype of name is",type(name))
+print("age:",age)
+print("datatype of age is",type(age))
+print("student:",student)
+print("datatype of student is",type(student))
+print("weight:",weight)
+print("datatype of weight is",type(weight))
+print("after typecasting")
+age=str(age)
+weight=int(weight)
+print("datatype of age is",type(age))
+print("datatype of weight is",type(weight))
+print("age:",age)
+print("weight:",weight)
