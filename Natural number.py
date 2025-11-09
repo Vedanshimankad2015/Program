99a52a3e24023112d56c42f5ee3245b7876c68d1
Vedanshimankad2015 <vedanshimankad11@gmail.com>
2025-11-09 22:46:33 +0530

    Natural number.py

---
+++ Natural number.py
@@ -0,0 +1,11 @@
+number = int(input("Enter Number to check"))
+print("Number to be checked:",number)
+
+if number<0:
+    print("This is not a natural number")
+
+elif number>0:
+    print("This is a natural number")
+
+else:
+    print("This is a natural number")
