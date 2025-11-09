760a703763e5184fa31bcc8ec4155036ca0690e4
Vedanshimankad2015 <vedanshimankad11@gmail.com>
2025-11-09 15:47:43 +0530

    Conversation with strings.py

---
+++ Conversation with strings.py
@@ -0,0 +1,9 @@
+first_name = "Hello"
+last_name = "World"
+full_name = first_name+last_name
+
+print("First Name :", first_name)
+print("Last Name :", last_name)
+print("Full Name :", full_name)
+
+word = 'hello'
