9ef25df29d0222ee3e7b4320c49a25e26e203003
Vedanshimankad2015 <vedanshimankad11@gmail.com>
2025-11-16 16:07:40 +0530

    Absoulute value.py

---
+++ Absoulute value.py
@@ -0,0 +1,9 @@
+def absoulute_value(num):
+
+    if num>=0:
+        return num
+    else:
+        return -num
+print("absoulute value of 89:,absoulue value(89)")
+
+print ("absoulute value of -189:,absoulue value(-189)")
