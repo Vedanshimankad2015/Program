c2a8d5f36318d71e3bb1a41a9b057365d213d5b9
Vedanshimankad2015 <vedanshimankad11@gmail.com>
2025-12-18 12:30:34 +0530

    Slam book.py

--- first folder/Slam book.py
+++ first folder/Slam book.py
@@ -18,23 +18,17 @@ while True:
     print("Favourite Color:", color)
     print("Favourite Food:", food)
     print("Message:", message)
-
+    print("part-2")
     choice = input("\nAdd another friend? (yes/no): ")
     if choice.lower() != "yes":
         break
-
-print("\n📕 Slam Book Closed. Thank you!")
-print("-----------------------------")
-
-while True:
-    print("\nNew Friend Entry")
-
+    print("\n Slam Book Closed. Thank you!")
+    print("-----------------------------")
     name = input("Your Name: ")
     nickname = input("Your Nickname: ")
     color = input("Favourite Color: ")
     food = input("Favourite Food: ")
     message = input("One Message for Me: ")
-
     print("\n✨ Slam Book Entry ✨")
     print("--------------------")
     print("Name:", name)
@@ -47,4 +41,4 @@ while True:
     if choice.lower() != "yes":
         break
 
-print("\n📕 Slam Book Closed. Thank you!")
+print("\n Slam Book Closed. Thank you!")
